b7b15975dd4584806246d9f7f1dcb912ec50aa4f
hesam <bhesam2000@yahoo.com>
2024-01-26 20:18:18 +0330

    create a text input and button for front

--- newsclassifier/src/App.jsx
+++ newsclassifier/src/App.jsx
@@ -1,5 +1,29 @@
+import { useRef } from "react";
+import './App.css';
 function App() {
-  return <></>;
+  const inputRef = useRef(null);
+  const handleClick = async () => {
+    const text = inputRef.current.value;
+    const result = await fetch("http://127.0.0.1:8000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+        title: "hello from front",
+      }),
+    });
+    console.error(result);
+  };
+
+  return (
+    <div
+    className="container">
+      <textarea ref={inputRef} type="text" />
+      <button onClick={handleClick}>send</button>
+    </div>
+  );
 }
 
 export default App;
