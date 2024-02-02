b7f8c9cf586ef562b9f0946179ebd98dd2408545
hesam <bhesam2000@yahoo.com>
2024-02-02 21:06:12 +0330

    completed ui

--- newsclassifier/src/App.jsx
+++ newsclassifier/src/App.jsx
@@ -1,27 +1,68 @@
-import { useRef } from "react";
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
+import bg from "../assets/bg.png";
 function App() {
   const inputRef = useRef(null);
+  const [isSending, setisSending] = useState(false);
+  const [resultclass, setResultClass] = useState(null);
+  const classes = {
+    "cultural and artistic": "فرهنگی و هنری",
+    economic: "اقتصادی",
+    environment: "محیط زیست",
+    miscellaneous: "متفرقه",
+    political: "سیاسی",
+    social: "اجتماعی",
+    sports: "ورزشی",
+    technological: "فناوری",
+    tourism: "جهان و گردشگری",
+  };
   const handleClick = async () => {
     const text = inputRef.current.value;
-    const result = await fetch("http://127.0.0.1:8000/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        text,
-        title: "hello from front",
-      }),
-    });
-    console.error(result);
+    setisSending(true);
+    try {
+      const result = await fetch("http://127.0.0.1:8000/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text,
+          title: "hello from front",
+        }),
+      });
+      console.error("zarp", result);
+      result.json().then((data) => {
+        setisSending(false);
+        setResultClass(classes[data]);
+      });
+    } catch {
+      setisSending(false);
+    }
   };
 
   return (
-    <div
-    className="container">
-      <textarea ref={inputRef} type="text" />
-      <button onClick={handleClick}>send</button>
+    <div className="main">
+      {/* <div className="left-container"></div> */}
+      <div className="middle-container">
+        <textarea
+          className="scroll-container"
+          ref={inputRef}
+          type="text"
+          placeholder="متن خبر را وارد کنید"
+        />
+        <button onClick={handleClick}>ارسال</button>
+        {resultclass && (
+          <h3>
+            دسته بندی : <span>{resultclass}</span>
+          </h3>
+        )}
+        {isSending && (
+          <div className="waiting">
+            <div className="loader"></div>
+          </div>
+        )}
+      </div>
+      {/* <div className="right-container"></div> */}
     </div>
   );
 }
